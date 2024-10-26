ce6048732131825cb4a5bb4a29b916687a0aabb7
cedekpoole <cameron.edek.poole@gmail.com>
2024-10-26 19:38:59 +0100

    separate app layout content into components

---
+++ src/components/Map.jsx
@@ -0,0 +1,15 @@
+function Map() {
+  return (
+    <main className="w-full lg:w-2/3 p-6">
+      <h1 className="text-3xl font-bold mb-6">Main Content</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+        imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae
+        scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec
+        congue eget, auctor vitae massa.
+      </p>
+    </main>
+  );
+}
+
+export default Map;
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,18 +1,11 @@
+import Map from "../components/Map";
 import Sidebar from "../components/SideBar";
 
 function AppLayout() {
   return (
     <div className="flex flex-col lg:flex-row lg:space-x-6 min-h-screen">
       <Sidebar />
-      <main className="w-full lg:w-2/3 p-6">
-        <h1 className="text-3xl font-bold mb-6">Main Content</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-          imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae
-          scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices
-          nec congue eget, auctor vitae massa.
-        </p>
-      </main>
+      <Map />
     </div>
   );
 }
