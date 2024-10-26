5dcfc89e54118bc244c314795139eff410f72264
cedekpoole <cameron.edek.poole@gmail.com>
2024-10-26 14:01:25 +0100

    create product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,27 @@
-function Product() {
+export default function Product() {
   return (
-    <div>
-      <h1 className="text-5xl">Product</h1>
-    </div>
+    <main className="p-3">
+      <section className="mx-auto grid grid-cols-1 md:grid-cols-2 gap-16 lg:gap-24 items-center">
+        <img
+          src="product.jpg"
+          alt="map on table"
+          className="w-full max-w-full lg:max-w-md rounded-lg"
+        />
+        <div>
+          <h2 className="text-4xl leading-tight mb-12">About GlobeTrotter.</h2>
+          <p className="text-lg mb-8">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo est
+            dicta illum vero culpa cum quaerat architecto sapiente eius non
+            soluta, molestiae nihil laborum, placeat debitis, laboriosam at fuga
+            perspiciatis?
+          </p>
+          <p className="text-lg mb-8">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
+            doloribus libero sunt expedita ratione iusto, magni, id sapiente
+            sequi officiis et.
+          </p>
+        </div>
+      </section>
+    </main>
   );
 }
-
-export default Product;
