cdf5e1a8b7ca7efd35ea2a47f7fced29a3bfa339
cedekpoole <cameron.edek.poole@gmail.com>
2024-10-26 14:07:53 +0100

    style pricing page

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,9 +1,24 @@
-function Pricing() {
+export default function Product() {
   return (
-    <div>
-      <h1 className="text-5xl">Pricing</h1>
-    </div>
+    <main className="p-3 mt-10">
+      <section className="mx-auto grid grid-cols-1 md:grid-cols-2 gap-16 lg:gap-24 items-center">
+        <div>
+          <h2 className="text-4xl leading-tight mb-12 font-bold">
+            Simple pricing.
+            <span className="block font-normal mt-2">Just £9/month.</span>
+          </h2>
+          <p className="text-lg">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae vel
+            labore mollitia iusto. Recusandae quos provident, laboriosam fugit
+            voluptatem iste.
+          </p>
+        </div>
+        <img
+          src="pricing.jpg"
+          alt="a map with pins in particular cities"
+          className="w-full rounded-lg"
+        />
+      </section>
+    </main>
   );
 }
-
-export default Pricing;
