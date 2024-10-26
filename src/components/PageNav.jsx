0dc546960b75c87b65c4733c1e921d17ddcc78c6
cedekpoole <cameron.edek.poole@gmail.com>
2024-10-26 13:27:07 +0100

    improve navbar styling

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -3,20 +3,22 @@ import logo from "../assets/GlobeTrotter.svg";
 
 function PageNav() {
   return (
-    <nav className="flex justify-between items-center py-3 bg-[#242424] font-bold text-xl">
+    <nav className="flex justify-between items-center py-5 bg-[#242424] font-medium text-lg">
       <NavLink to="/">
         <img src={logo} />
       </NavLink>
-      <ul className="flex w-1/2 justify-between p-5">
-        <li>
-          <NavLink to="/pricing">Pricing</NavLink>
+      <ul className="flex w-1/2 justify-between items-center">
+        <li className="hover:text-[#E2B07E]">
+          <NavLink to="/product">PRODUCT</NavLink>
         </li>
-        <li>
-          <NavLink to="/product">Product</NavLink>
+        <li className="hover:text-[#E2B07E]">
+          <NavLink to="/pricing">PRICING</NavLink>
         </li>
-        <li>
-          <NavLink to="/login">Login</NavLink>
-        </li>
+        <NavLink to="/login">
+          <li className="px-6 py-2 rounded-full bg-[#A6714D] text-[#F3E9DC] hover:bg-[#91603C] transition-colors duration-300 ease-in-out shadow-md">
+            LOGIN
+          </li>
+        </NavLink>
       </ul>
     </nav>
   );
