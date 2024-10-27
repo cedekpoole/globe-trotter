be12435346a36c8f94585a2f8bda50774e07e047
cedekpoole <cameron.edek.poole@gmail.com>
2024-10-27 14:33:18 +0000

    add dynamic city route + create city component

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import CityList from "./components/CityList";
 import CountriesList from "./components/CountriesList";
+import City from "./components/City";
 
 const protectedRoutes = ["/app", "/app/cities", "/app/countries", "/app/form"];
 const BASE_URL = "http://localhost:8000/";
@@ -55,6 +56,7 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+          <Route path="cities/:id" element={<City />} />
           <Route
             path="countries"
             element={<CountriesList cities={cities} isLoading={isLoading} />}
---
+++ src/components/City.jsx
@@ -0,0 +1,13 @@
+function City() {
+  const currentCity = {
+    cityName: "City Name",
+    date: "2027-10-3T10:00:00",
+    emoji: "🏙",
+    notes: "City notes",
+  };
+
+  const { cityName, date, emoji, notes } = currentCity;
+  return <div></div>;
+}
+
+export default City;
