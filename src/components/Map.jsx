e4784b61d6a8451b6c866bb720ea6d295378404e
cedekpoole <cameron.edek.poole@gmail.com>
2024-10-28 17:34:22 +0000

    add markers for each city in cities context

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
   const navigate = useNavigate();
+
+  const { cities } = useCities();
+
   const [mapPosition, setMapPosition] = useState({ lat: 51.505, lng: -0.09 });
   const [searchParams, setSearchParams] = useSearchParams();
 
@@ -16,18 +20,21 @@ function Map() {
       <MapContainer
         center={mapPosition}
         zoom={13}
-        scrollWheelZoom={false}
+        scrollWheelZoom={true}
         className=" min-h-[400px] h-full w-full"
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a>, &copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>{city.cityName}</Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </main>
   );
