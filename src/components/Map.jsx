41a23bacfd62c17d169ebc9a5f7866827d4c5530
cedekpoole <cameron.edek.poole@gmail.com>
2024-10-28 18:46:19 +0000

    create changecenter and detect click component

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,20 +1,33 @@
-import { useState } from "react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useEffect, useState } from "react";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { useCities } from "../contexts/CitiesContext";
+import PropTypes from "prop-types";
+
+ChangeCenter.propTypes = {
+  position: PropTypes.array.isRequired,
+};
 
 function Map() {
-  const navigate = useNavigate();
-
   const { cities } = useCities();
 
-  const [mapPosition, setMapPosition] = useState({ lat: 51.505, lng: -0.09 });
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [mapPosition, setMapPosition] = useState([51.505, -0.09]);
+  const [searchParams] = useSearchParams();
 
-  const coordinates = {
-    lat: searchParams.get("lat") || 0,
-    lng: searchParams.get("lng") || 0,
-  };
+  const mapLat = parseFloat(searchParams.get("lat"));
+  const mapLng = parseFloat(searchParams.get("lng"));
+
+  useEffect(() => {
+    if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+  }, [mapLat, mapLng]);
+
   return (
     <main className="w-full lg:w-2/3 p-6 h-screen flex-1 relative">
       <MapContainer
@@ -38,9 +51,23 @@ function Map() {
             </Popup>
           </Marker>
         ))}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
       </MapContainer>
     </main>
   );
 }
 
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
+
+function DetectClick() {
+  const navigate = useNavigate();
+  useMapEvents({
+    click: (e) => navigate(`/app/form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
+  });
+}
 export default Map;
