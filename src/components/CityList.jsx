e0644cddc74ff870b89bd71a680284645462bbe5
cedekpoole <cameron.edek.poole@gmail.com>
2024-10-29 13:28:43 +0000

    edit max height

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -15,7 +15,7 @@ function CityList() {
       </Message>
     );
   return (
-    <ul className="flex flex-col gap-2 max-h-[300px] overflow-auto">
+    <ul className="flex flex-col gap-2 max-h-[358px] overflow-auto">
       {cities.map((city) => (
         <CityItem key={city.id} city={city} />
       ))}
