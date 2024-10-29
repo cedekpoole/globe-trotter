a1c5faa80f981853c895c274ec155871a03f3280
cedekpoole <cameron.edek.poole@gmail.com>
2024-10-29 19:26:42 +0000

    add prop types

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -1,6 +1,11 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
+import PropTypes from "prop-types";
+
+ProtectedRoute.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 function ProtectedRoute({ children }) {
   const { isAuthenticated } = useAuth();
